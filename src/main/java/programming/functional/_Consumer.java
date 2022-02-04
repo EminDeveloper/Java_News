9fd22f2e928841afa3a5f62613c048203efc87f0
Emin <emin.f.mammadov@gmail.com>
2022-02-05 00:56:01 +0400

    BiConsumer

--- src/main/java/programming/functional/_Consumer.java
+++ src/main/java/programming/functional/_Consumer.java
@@ -1,5 +1,6 @@
 package programming.functional;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
@@ -8,10 +9,17 @@ public class _Consumer {
         // Normal java Function
         Customer maria = new Customer("Maria", "99999");
         greetCustomer(maria);
-
+        greetCustomerV2(maria, true);
         // Consumer Functional interface
         greetCustomerConsumer.accept(maria);
+
+        greetCustomerConsumerV2.accept(maria, false);
     }
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 = (customer, showPhoneNumber) ->
+            System.out.println("Hello " + customer.customerName +
+                    ", thanks for registering phone number "
+                    + (showPhoneNumber ? customer.customerPhoneNumber : "*********"));
 
     static Consumer<Customer> greetCustomerConsumer = customer ->
             System.out.println("Hello " + customer.customerName +
@@ -22,6 +30,12 @@ public class _Consumer {
         System.out.println("Hello " + customer.customerName +
                 ", thanks for registering phone number "
                 + customer.customerPhoneNumber);
+    }
+
+    static void greetCustomerV2(Customer customer, boolean showPhoneNumber){
+        System.out.println("Hello " + customer.customerName +
+                ", thanks for registering phone number "
+                + (showPhoneNumber ? customer.customerPhoneNumber : "*********"));
     }
 
     static class Customer{
